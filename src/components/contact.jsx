2badc3cfff4c7e79433bfcf4987b310136797782
Hamadoun Sankare <husumaci@gmail.com>
2021-01-09 10:59:02 +0000

    make it better

---
+++ src/components/contact.jsx
@@ -0,0 +1,131 @@
+import React, { Component } from "react";
+
+export class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <div id="contact">
+          <div className="container">
+            <div className="col-md-8">
+              <div className="row">
+                <div className="section-title">
+                  <h2>Contactez-Nous</h2>
+                  <p>
+                    Veuillez remplir les champs ci-dessous afin de nous envoyer un email. Nous vous reviendrons dans les plus brefs delais.
+                  </p>
+                </div>
+                <form name="sentMessage" id="contactForm" noValidate>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          id="name"
+                          className="form-control"
+                          placeholder="Nom"
+                          required="required"
+                        />
+                        <p className="help-block text-danger"></p>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="email"
+                          id="email"
+                          className="form-control"
+                          placeholder="Email"
+                          required="required"
+                        />
+                        <p className="help-block text-danger"></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <textarea
+                      name="message"
+                      id="message"
+                      className="form-control"
+                      rows="4"
+                      placeholder="Message"
+                      required
+                    ></textarea>
+                    <p className="help-block text-danger"></p>
+                  </div>
+                  <div id="success"></div>
+                  <button type="submit" className="btn btn-custom btn-lg">
+                    Envoyer
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="col-md-3 col-md-offset-1 contact-info">
+              <div className="contact-item">
+                <h3>Contact Info</h3>
+                <p>
+                  <span>
+                    <i className="fa fa-map-marker"></i> Addresse
+                  </span>
+                  {this.props.data ? this.props.data.address : "loading"}
+                </p>
+              </div>
+              <div className="contact-item">
+                <p>
+                  <span>
+                    <i className="fa fa-phone"></i> Telephone
+                  </span>{" "}
+                  {this.props.data ? this.props.data.phone : "loading"}
+                </p>
+              </div>
+              <div className="contact-item">
+                <p>
+                  <span>
+                    <i className="fa fa-envelope-o"></i> Email
+                  </span>{" "}
+                  {this.props.data ? this.props.data.email : "loading"}
+                </p>
+              </div>
+            </div>
+            <div className="col-md-12">
+              <div className="row">
+                <div className="social">
+                  <ul>
+                    <li>
+                      <a
+                        href={this.props.data ? this.props.data.facebook : "/"}
+                      >
+                        <i className="fa fa-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href={this.props.data ? this.props.data.twitter : "/"}>
+                        <i className="fa fa-twitter"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href={this.props.data ? this.props.data.youtube : "/"}>
+                        <i className="fa fa-youtube"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="footer">
+          <div className="container text-center">
+            <p>
+              &copy; 2021 Société Diagana Nouvelle Vision S.A.R.L. Design by{" "}
+              <a href="http://www.templatewire.com" rel="nofollow">
+                Tech-Up
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contact;
